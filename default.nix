c961ca9788fe20862cf4e076e7cd4bc8bba9c21d
Roel van Dijk <roel@lambdacube.nl>
2015-12-18 13:18:45 +0100

    new hs-vision-playground
    - Separated from hs-libs/thea

--- default.nix
+++ default.nix
@@ -3,58 +3,40 @@
 
 , opencv
 
-, attoparsec
 , base
 , bindings-DSL
 , bytestring
-, conduit
-, conduit-extra
 , containers
-, exceptions
-, http-conduit
 , inline-c
 , inline-c-cpp
 , lens
 , linear
 , lumi-hackage-extended
-, optparse-applicative
 , primitive
-, resourcet
 , template-haskell
 , text
-, transformers
 , vector
 }:
 mkDerivation {
   pname = "thea";
   version = "0.0.0";
   src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends =
-    [ attoparsec
-      base
+  libraryHaskellDepends =
+    [ base
       bindings-DSL
       bytestring
-      conduit
-      conduit-extra
       containers
-      exceptions
-      http-conduit
       inline-c
       inline-c-cpp
       lens
       linear
       lumi-hackage-extended
-      optparse-applicative
       primitive
-      resourcet
       template-haskell
       text
-      transformers
       vector
     ];
-  executablePkgconfigDepends = [ opencv ];
+  libraryPkgconfigDepends = [ opencv ];
   configureFlags =
     [ "--with-gcc=g++"
       "--with-ld=g++"
