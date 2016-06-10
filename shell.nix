e1e86c0e8631c824328110bdd9d9076f1b6b7db8
Bas van Dijk <v.dijk.bas@gmail.com>
2016-06-10 13:36:15 +0200

    Fix the opencv-3.1.0 nix dependency

--- shell.nix
+++ shell.nix
@@ -7,6 +7,16 @@ let
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage (import ./haskell-opencv.nix) {};
+  drv = haskellPackages.callPackage (import ./haskell-opencv.nix) {
+    opencv3_1 = pkgs.callPackage ./opencv-3.1.0.nix {
+      gtk = pkgs.gtk3;
+      qt  = pkgs.qt5;
+      enableIpp     = true;
+      enableContrib = true;
+      enableBloat   = true;
+      enableOpenGL  = true;
+      enableQT      = false;
+    };
+  };
 
 in if pkgs.lib.inNixShell then drv.env else drv
