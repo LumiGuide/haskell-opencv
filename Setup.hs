d84f33e2c8e4b38a1866016f3305d987ca73ff9b
Timo von Holtz <vonholtz@cpmed.com>
2016-07-22 14:00:53 +0200

    Add custom Setup.hs
    Set linker and compiler to g++

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,8 @@
 import Distribution.Simple
-main = defaultMain
+import System.Environment
+import System.IO
+
+main =
+    do args <- getArgs
+       let args' = if "configure" `elem` args then args ++ ["--with-gcc","g++","--with-ld","g++"] else args
+       defaultMainArgs args'
