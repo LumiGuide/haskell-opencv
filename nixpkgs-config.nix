703489f4d79d40db00bdc04d616256592415864c
Bas van Dijk <v.dijk.bas@gmail.com>
2016-09-03 16:55:16 +0200

    Preparations for a Hackage release

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -1,29 +1,31 @@
-let
-  haskellOverrides = {
-    overrides = self: super: {
-      inline-c = super.inline-c.override (args: args // {
-        mkDerivation = expr: args.mkDerivation (expr // {
-          version = "0.5.5.5";
-          sha256 = "0c3jijav2iz9b5k1hzzidq7rbavj45rbbyrk93ybd2dagrj45lgk";
-        });
-      });
-      opencv = self.callPackage (import ./haskell-opencv.nix) {};
-    };
-  };
-  osx = builtins.currentSystem == "x86_64-darwin";
-in {
-  packageOverrides = super: {
-    haskellPackages = super.haskellPackages.override haskellOverrides;
+{
+  packageOverrides = pkgs:
+    let
+      haskellOverrides = {
+        overrides = self: super: {
+          inline-c = super.inline-c.override (args: args // {
+            mkDerivation = expr: args.mkDerivation (expr // {
+              version = "0.5.5.5";
+              sha256 = "0c3jijav2iz9b5k1hzzidq7rbavj45rbbyrk93ybd2dagrj45lgk";
+            });
+          });
+          opencv = /* TODO (BvD): Get this to work: pkgs.haskell.lib.buildStrictly */
+                   (self.callPackage (import ./haskell-opencv.nix) {});
+        };
+      };
+      osx = builtins.currentSystem == "x86_64-darwin";
+    in {
+      haskellPackages = pkgs.haskellPackages.override haskellOverrides;
 
-    opencv3_1 = super.callPackage ./opencv-3.1.0.nix
-                                # ./opencv-HEAD.nix
-    { gtk = super.gtk3;
-      qt  = super.qt5;
-      enableIpp     = !osx;
-      enableContrib = true;
-      enableBloat   = !osx;
-      enableOpenGL  = true;
-      enableQT      = false;
+      opencv3_1 = pkgs.callPackage ./opencv-3.1.0.nix
+                                  # ./opencv-HEAD.nix
+      { gtk = pkgs.gtk3;
+        qt  = pkgs.qt5;
+        enableIpp     = !osx;
+        enableContrib = true;
+        enableBloat   = !osx;
+        enableOpenGL  = true;
+        enableQT      = false;
+      };
     };
-  };
 }
