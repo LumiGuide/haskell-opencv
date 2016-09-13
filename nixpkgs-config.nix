b0cf7baced281e607eb263664a46ebbc059762b2
Bas van Dijk <v.dijk.bas@gmail.com>
2016-09-13 02:30:28 +0200

    Upgrade inline-c to 0.5.5.7

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -5,8 +5,8 @@
         overrides = self: super: {
           inline-c = super.inline-c.override (args: args // {
             mkDerivation = expr: args.mkDerivation (expr // {
-              version = "0.5.5.5";
-              sha256 = "0c3jijav2iz9b5k1hzzidq7rbavj45rbbyrk93ybd2dagrj45lgk";
+              version = "0.5.5.7";
+              sha256 = "1falgh6ahyr9yb4f8npz0hai5yy3bf0vza5s90al9ngf6wf5d85p";
             });
           });
           opencv = /* TODO (BvD): Get this to work: pkgs.haskell.lib.buildStrictly */
