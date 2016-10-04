41a5403c3899b7fe715b6da40c55ecb1e4fded27
Marcin Tolysz <tolysz@gmail.com>
2016-10-04 20:00:38 +0100

    Find face, for eyes look on it

--- examples/src/face-detect.hs
+++ examples/src/face-detect.hs
@@ -1,12 +1,16 @@
 {-# language DataKinds #-}
 {-# language FlexibleInstances #-}
 {-# language TemplateHaskell #-}
+{-# language TupleSections #-}
+{-# language ViewPatterns #-}
+{-# language FlexibleContexts #-}
 
 import qualified OpenCV as CV
 import qualified OpenCV.Internal.Core.Types.Mat as CV
 import OpenCV.VideoIO.Types
 import GHC.Word (Word8)
 import OpenCV.Example
+import qualified Data.Vector as V
 import OpenCV.TypeLevel
 import Linear.V2 ( V2(..) )
 import Linear.V4 ( V4(..)  )
@@ -35,6 +39,8 @@ main = do
 
     minSize = Nothing :: Maybe (V2 Int32)
     maxSize = Nothing :: Maybe (V2 Int32)
+    addCorner (CV.fromRect -> CV.HRect (V2 tx ty) _) (CV.fromRect -> CV.HRect (V2 ex ey) b) =
+         CV.toRect (CV.HRect (V2 (tx + ex) (ty + ey)) b)
 
     loop cap param@(ccFrontal, ccEyes, w, h) window = do
       _ok <- CV.videoCaptureGrab cap
@@ -45,15 +51,16 @@ main = do
           let img' :: CV.Mat ('S ['D, 'D]) ('S 3) ('S Word8)
               img' = CV.exceptError $ CV.coerceMat img
               imgGray = CV.exceptError $ CV.cvtColor CV.bgr CV.gray img'
+
               faces = ccDetectMultiscale ccFrontal imgGray
-              -- eyes have to be on the face ! fix
-              eyes = ccDetectMultiscale ccEyes     imgGray
-          print faces
+              eyedFaces = V.concat . V.toList . CV.exceptError
+                $ mapM (\f -> V.map (addCorner f) . ccDetectMultiscale ccEyes
+                <$> CV.matSubRect imgGray f) faces
 
           let box = CV.exceptError $
                 CV.withMatM (h ::: w ::: Z) (Proxy :: Proxy 3) (Proxy :: Proxy Word8) white $ \imgM -> do
                   void $ CV.matCopyToM imgM (V2 0 0) (CV.unsafeCoerceMat img) Nothing
-                  forM_ eyes  $ \eyeRect  -> lift $ CV.rectangle imgM eyeRect green 2 CV.LineType_8 0
+                  forM_ eyedFaces $ \eyeRect  -> lift $ CV.rectangle imgM eyeRect green 2 CV.LineType_8 0
                   forM_ faces $ \faceRect -> lift $ CV.rectangle imgM faceRect blue 2 CV.LineType_8 0
           CV.imshow window ( CV.unsafeCoerceMat box )
 
