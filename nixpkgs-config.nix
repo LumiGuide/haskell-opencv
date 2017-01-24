5512805cf20b0cb9d6f1c75d77725e31ead1f2b8
Bas van Dijk <v.dijk.bas@gmail.com>
2017-01-24 09:44:07 +0100

    Don't override inline-c anymore
    A recent enough version is now in nixpkgs

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -3,12 +3,6 @@
     let
       haskellOverrides = {
         overrides = self: super: {
-          inline-c = super.inline-c.override (args: args // {
-            mkDerivation = expr: args.mkDerivation (expr // {
-              version = "0.5.5.7";
-              sha256 = "1falgh6ahyr9yb4f8npz0hai5yy3bf0vza5s90al9ngf6wf5d85p";
-            });
-          });
           opencv = /* TODO (BvD): Get this to work: pkgs.haskell.lib.buildStrictly */
                    (self.callPackage (import ./haskell-opencv.nix) {});
         };
