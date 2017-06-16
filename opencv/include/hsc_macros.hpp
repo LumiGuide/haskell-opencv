884857725b02e8a930a2a4178d8e39e372585131
Roel van Dijk <roel@lambdacube.nl>
2017-06-16 15:28:28 +0200

    Add hsc2hs alignof macro

--- opencv/include/hsc_macros.hpp
+++ opencv/include/hsc_macros.hpp
@@ -9,6 +9,7 @@ https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/utils.html#custo
 #include <bindings.dsl.h>
 
 #define bc_sizeof_varid(name) {printf("c'sizeof_");bc_word(name);}; \
+#define bc_alignof_varid(name) {printf("c'alignof_");bc_word(name);}; \
 
 /*
 The #sizeof macro outputs a Haskell constant with the size of the C/C++ type in bytes.
@@ -25,6 +26,25 @@ Results in the following Haskell code:
 #define hsc_sizeof(name) \
   { bc_sizeof_varid(# name);printf(" :: Int\n"); \
     bc_sizeof_varid(# name);printf(" = %lu\n", sizeof(name)); \
+  }; \
+
+/*
+The #alignof macro outputs a Haskell constant with the alignment off the C/C++ type in bytes.
+
+For example the following:
+
+  #alignof Point2i
+
+Results in the following Haskell code:
+
+  c'alignof_Point2i :: Int
+  c'alignof_Point2i = 4
+
+The specific alignment that is calculated is platform dependent.
+*/
+#define hsc_alignof(name) \
+  { bc_alignof_varid(# name);printf(" :: Int\n"); \
+    bc_alignof_varid(# name);printf(" = %lu\n", alignof(name)); \
   }; \
 
 #endif /* __HSC_MACROS_H__ */
