916001562c6295d229c9dd846f859af5e00e6a1e
Bas van Dijk <v.dijk.bas@gmail.com>
2017-10-27 13:33:19 +0200

    nixpkgs.nix: upgrade to the latest nixos-unstable
    this includes my:
    
      Author: Bas van Dijk <v.dijk.bas@gmail.com>
      Date:   Fri Oct 20 18:36:37 2017 +0200
    
          opencv: 3.2.0 -> 3.3.0

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -2,6 +2,6 @@ let pkgs = import <nixpkgs> {};
 in pkgs.fetchFromGitHub {
  owner   = "NixOS";
  repo    = "nixpkgs";
- rev     = "797d4d4a3f38244ae5e5b5b55b7304ab6e75fe51";
- sha256  = "0hbrcmyq2dpgynhid702gxlbcbhf4ljjp2rii56lr7fyybnj1fc7";
+ rev     = "66b63d2f5a442d642ac2b226d71b4f97bafce5c8";
+ sha256  = "0vss6g2gsirl2ds3zaxwv9sc6q6x3zc68431z1wz3wpbhpw190p5";
 }
