04897fa9717642bd2a0eb42fd84741c09b62d325
Bas van Dijk <v.dijk.bas@gmail.com>
2017-11-05 15:47:59 +0100

    nixpkgs.nix: upgrade to latest master

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -2,6 +2,6 @@ let pkgs = import <nixpkgs> {};
 in pkgs.fetchFromGitHub {
  owner   = "NixOS";
  repo    = "nixpkgs";
- rev     = "66b63d2f5a442d642ac2b226d71b4f97bafce5c8";
- sha256  = "0vss6g2gsirl2ds3zaxwv9sc6q6x3zc68431z1wz3wpbhpw190p5";
+ rev     = "cdbe4671bef1c0ba6345c51270472998bd44e266";
+ sha256  = "0g6296b36ig48j785cpfbc0avzk1fjlba1izbagjmk30ssgw6vk9";
 }
