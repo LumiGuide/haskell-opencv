ca605b6139f733df3722d37792b8ebd4c2b1f1a7
Bas van Dijk <v.dijk.bas@gmail.com>
2018-01-01 18:01:31 +0100

    nixpkgs-version.nix: upgrade to the lastest master

--- nixpkgs-version.nix
+++ nixpkgs-version.nix
@@ -1,4 +1,4 @@
 {
-  rev    = "79081322ab97b7d9c937c07b24cd0911e2a3a0a9";
-  sha256 = "1jcygx2mxmrzqipn5rdcip1lvzvnf4a48s6yv22s0fypplmfxzcb";
+  rev    = "4d917639fb3c2959f058e533d5ebb2001e5b05fa";
+  sha256 = "17l2cqprr7zz2mr3wsajp7cw0dfw2rcllg6imd7igqy70x0s0a03";
 }
