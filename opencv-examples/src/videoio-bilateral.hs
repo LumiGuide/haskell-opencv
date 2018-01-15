5c27a860688e40234644bf5aa5e7fcf22031f45e
Roel van Dijk <roel@lambdacube.nl>
2018-01-15 20:37:01 +0100

    Fix videoio-bilateral warnings

--- opencv-examples/src/videoio-bilateral.hs
+++ opencv-examples/src/videoio-bilateral.hs
@@ -13,7 +13,6 @@ import OpenCV.TypeLevel
 import OpenCV.Example
 import OpenCV.VideoIO.Types as T
 import System.Environment
-import System.Exit
 import Data.Word
 import Control.Monad
 import Text.Read
@@ -21,7 +20,7 @@ import Text.Read
 main :: IO ()
 main = do
     args <- getArgs
-    when (length args /= 2) $ print "videoio-bilateral input d output.mov "
+    when (length args /= 2) $ print ("videoio-bilateral input d output.mov " :: String)
     let (_:d:output:_) = args ++ ["5", "/dev/null"]
     cap <- createCaptureArg
 
