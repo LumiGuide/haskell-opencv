7adff8d5cf4eb6af7fa07e08731a89999b242b27
Bas van Dijk <v.dijk.bas@gmail.com>
2018-01-22 19:43:05 +0100

    WIP

--- opencv/Setup.hs
+++ opencv/Setup.hs
@@ -1,8 +1,25 @@
-import Distribution.Simple ( defaultMainArgs )
+import Distribution.Simple
+import Distribution.Simple.Setup
+import Distribution.Types.PackageDescription ( PackageDescription )
+import Distribution.Types.LocalBuildInfo ( LocalBuildInfo )
 import System.Environment ( getArgs )
 
 main = do
     args <- getArgs
     let args' | "configure" `elem` args = args ++ ["--with-gcc","c++", "--with-ld","c++"]
               | otherwise               = args
-    defaultMainArgs args'
+    defaultMainWithHooksArgs userHooks args'
+  where
+    userHooks :: UserHooks
+    userHooks = simpleUserHooks{ haddockHook = myHaddockHook }
+
+    myHaddockHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> HaddockFlags -> IO ()
+    myHaddockHook pkgDesc lbi uk haddockFlags =
+        haddockHook simpleUserHooks pkgDesc lbi uk myHaddockFlags
+      where
+        myHaddockFlags :: HaddockFlags
+        myHaddockFlags = haddockFlags{ haddockProgramArgs = myHaddockProgramArgs}
+
+        myHaddockProgramArgs :: [(String, [String])]
+        myHaddockProgramArgs = haddockProgramArgs haddockFlags ++
+          [ ("--optghc", ["-pgmc=c++"]) ]
