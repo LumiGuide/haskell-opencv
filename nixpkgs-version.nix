3c8d0751e37a02e3f02c47389d633b2d09f3ec6b
Bas van Dijk <v.dijk.bas@gmail.com>
2018-05-22 00:45:57 +0200

    Upgrade the the latest nixpkgs master

--- nixpkgs-version.nix
+++ nixpkgs-version.nix
@@ -1,4 +1,4 @@
 {
-  rev    = "1d9330d63a5a30f4eb578eda7840be7b11ec6277"; # nixos-unstable
-  sha256 = "1zrvi9nal5hzqq87slcm8p6dsjqczrrzaqyxda8rcpa5lb0kl1ky";
+  rev    = "07723f14bb132f6d65c74b953bef39cd80457273";
+  sha256 = "1aw5fs47hby67klg8h4awaqajcgpr60xhwyc84s51my8vr4xrs84";
 }
--- overlay.nix
+++ overlay.nix
@@ -99,7 +99,6 @@ let
 
     };
   };
-  osx = builtins.currentSystem == "x86_64-darwin";
 in  {
   haskellPackages = previous.haskellPackages.override haskellOverrides;
   haskell = previous.haskell // {
@@ -109,26 +108,11 @@ in  {
       ghc842 = previous.haskell.packages.ghc842.override {
         overrides = self: super:
           haskellOverrides.overrides self super // (with previous.haskell.lib; {
-            inline-c = super.inline-c_0_6_0_6;
             criterion = super.criterion_1_4_1_0;
             base-compat-batteries = doJailbreak super.base-compat-batteries;
             base-compat = super.base-compat_0_10_1;
           });
       };
     };
-  };
-
-  opencv3 = previous.opencv3.override {
-    enableIpp       = true;
-    enableContrib   = true;
-    enableGtk3      = true;
-    enableFfmpeg    = !osx;
-    enableGStreamer = true;
-    enableDocs      = true;
-    enableUnfree    = false;
-    enableTesseract = true;
-    enableOvis      = false; # Currently causes the error: "Missing C library: opencv_ovis"
-    enableGPhoto2   = true;
-    enableDC1394    = false; # Currently causes the warning: libdc1394 error: Failed to initialize libdc1394
   };
 }
